d0b58677b1f268e5f7d17bc8d58a9eea789455c2
Karl Lu <karl.lu@outlook.com>
2023-08-24 22:04:44 -0400

    created custom post type program, created single and archive templates

--- archive-event.php
+++ archive-event.php
@@ -1,9 +1,6 @@
 <!-- designated archive template for events -->
 
-
-<?php
-get_header();
-?>
+<?php get_header(); ?>
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
     <div class="page-banner__content container container--narrow">
--- functions.php
+++ functions.php
@@ -37,6 +37,12 @@ function university_adjust_queries($query) {
         $query->set('order', 'ASC');
         $query->set('meta_query', [['key' => 'event_date', 'compare' => '>=', 'value' => $today, 'type' => 'numeric']]);
     }
+
+    if (!is_admin() && is_post_type_archive('program') && $query->is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1);
+    }
 }
 
 add_action('pre_get_posts', 'university_adjust_queries'); // calls right before a get post request
