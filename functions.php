1c27b4c3319805c5e2e87f1068490180ea8c7cb3
Karl Lu <karl.lu@outlook.com>
2023-08-25 16:38:35 -0400

    commented out unused dynamic menu, added thumbnail support w/mu-plugin

--- functions.php
+++ functions.php
@@ -16,10 +16,12 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features()
 {
-    register_nav_menu('headerMenuLocation', 'Header Menu Location'); // registers menu in WP menu page (called name in code, displayed name in UI)
-    register_nav_menu('footerLocationOne', 'Footer Menu Location One');
-    register_nav_menu('footerLocationTwo', 'Footer Menu Location Two');
+    // registers menu in WP menu page (called name in code, displayed name in UI)
+    // register_nav_menu('headerMenuLocation', 'Header Menu Location'); 
+    // register_nav_menu('footerLocationOne', 'Footer Menu Location One');
+    // register_nav_menu('footerLocationTwo', 'Footer Menu Location Two');
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
 }
 // calls function after setting up theme
 add_action('after_setup_theme', 'university_features');
